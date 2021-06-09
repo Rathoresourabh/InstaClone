3bb5c2d7cfe3f9545020d69bf3b584ab6b1ad274
Rathoresourabh <sourabhrathour1999@gmail.com>
2021-06-09 18:26:20 +0530

    first commit

---
+++ src/utils/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCX7jXXqWC1xPWiKL0Hga5DHekpu6TdaV8",
+  authDomain: "tryy-353df.firebaseapp.com",
+  projectId: "tryy-353df",
+  storageBucket: "tryy-353df.appspot.com",
+  messagingSenderId: "35421473127",
+  appId: "1:35421473127:web:bba2a688fcc1db8ab6c7c2"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
