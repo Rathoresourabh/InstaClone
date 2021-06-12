e47f1faa6141de472933ac69787e2c84b1c4aa68
Rathoresourabh <sourabhrathour1999@gmail.com>
2021-06-12 22:11:52 +0530

    updated add image

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,17 +1,26 @@
+import {useState} from 'react'
 import { Box } from "@material-ui/core";
-import firebase from "../utils/firebase";
+import ImageUploader from "react-images-upload"
+
 
 export default function Add() {
+  let [picture,setPicture] = useState();
+  let onDrop = function (picture) {
+
+    console.log(picture[picture.length -1]);
+    setPicture(picture[picture.length - 1]);
+  }
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
-      <h1>Add Page</h1>
-      <button
-        onClick={function () {
-          firebase.auth().signOut();
-        }}
-      >
-        Sign Out
-      </button>
+      <ImageUploader
+        withIcon={true}
+        buttonText="Choose Images"
+        onChange={onDrop}
+        imgExtension={[".jpg", ".jpeg", ".gif", ".png", ".gif"]}
+        maxFileSize={5242880}
+      />
+
+      {picture && <img src={URL.createObjectURL(picture)} alt="something" />}
     </Box>
   );
 }
